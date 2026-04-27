7f9c1b4b3b1dc5568914a03f213e81b5c25d1266
NGUYEN HUU KHANH HUNG <nhkhanhhung2005@gmail.com>
2026-04-27 22:59:17 +0700

    fix: update checkpoint paths for conditional pretraining on F1 dataset

---
+++ scripts/cond_pretrain_trial.py
@@ -0,0 +1,40 @@
+from rt.main import main
+from rt.tasks import all_tasks, forecast_tasks
+
+if __name__ == "__main__":
+    main(
+        # misc
+        project="rt",
+        eval_splits=["val", "test"],
+        eval_freq=100,
+        eval_pow2=True,
+        max_eval_steps=40,
+        load_ckpt_path="/content/drive/MyDrive/RelationalTransformer/pretrain/scarlet-mountain-18/rel-trial_study-outcome_best.pt",
+        save_ckpt_dir="ckpts/cond_pretrain/trial/",
+        # compile_=True,
+        compile_=False,
+        seed=0,
+        # data
+        train_tasks=[
+            t for t in all_tasks if t[0] == "rel-trial" and t[1] != "study-outcome"
+        ],
+        eval_tasks=[("rel-trial", "study-outcome", "outcome", [])],
+        batch_size=32,
+        num_workers=8,
+        max_bfs_width=256,
+        # optimization
+        lr=1e-4,
+        wd=0.0,
+        lr_schedule=False,
+        max_grad_norm=1.0,
+        # model
+        embedding_model="all-MiniLM-L12-v2",
+        d_text=384,
+        seq_len=1024,
+        num_blocks=12,
+        d_model=256,
+        num_heads=8,
+        d_ff=1024,
+        # max_steps=2**12 + 1,
+        epochs=1,
+    )
